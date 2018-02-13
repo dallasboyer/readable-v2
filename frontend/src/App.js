a5bbbee33c5158a24c48cbafecc199c6463c25fa
gratitude <master@gratitudes-MacBook-Pro.local>
2018-02-13 14:25:12 -0800

    Feat: added main route and 404

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react'
 // import './App.css'
+import { Switch, Route, /*withRouter*/ } from 'react-router-dom'
+import ViewAll from './components/ViewAll'
+import NotFound from './components/NotFound'
 
 class App extends Component {
+  componentDidMount(){
+    // TODO fetch all posts
+    // TODO fetch all categories
+  }
   render() {
     return (
       <div>
-        <h1>Readable V2</h1>
+        <header>
+          <h1>Readable V2</h1>
+        </header>
+        
+        <Switch>
+          <Route exact strict path="/" render={() => (
+            <ViewAll
+              // TODO posts={}
+            />
+          )} />
+          <Route path="*" render={() => (
+            <NotFound />
+          )} />
+        </Switch>
+
+        <footer>
+          Made by Dallas
+        </footer>
       </div>
     )
   }
 }
 
 export default App
+// export default withRouter(App)
---
+++ frontend/src/components/ViewAll.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ViewAll extends Component {
+  render() {
+    return (
+      <main>
+        <h1>All Posts</h1>
+      </main>
+    )
+  }
+}
+
+export default ViewAll
